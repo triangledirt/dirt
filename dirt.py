4728c79bfea6a7820efbc7837bc0af5e93ebfde5
dirt <dirtlove@proton.me>
2024-06-03 10:35:20 -0400

    Update dirt.py

--- dirt.py
+++ dirt.py
@@ -3,22 +3,22 @@ import random
 
 class game2:
 	data = [
-	[[0, 0], [0, 0]],
-	[[0, 0], [0, 1]],
-	[[0, 0], [1, 0]],
-	[[0, 0], [1, 1]],
-	[[0, 1], [0, 0]],
-	[[0, 1], [0, 1]],
-	[[0, 1], [1, 0]],
-	[[0, 1], [1, 1]],
-	[[1, 0], [0, 0]],
-	[[1, 0], [0, 1]],
-	[[1, 0], [1, 0]],
-	[[1, 0], [1, 1]],
-	[[1, 1], [0, 0]],
-	[[1, 1], [0, 1]],
-	[[1, 1], [1, 0]],
-	[[1, 1], [1, 1]],
+		[[0, 0], [0, 0]],
+		[[0, 0], [0, 1]],
+		[[0, 0], [1, 0]],
+		[[0, 0], [1, 1]],
+		[[0, 1], [0, 0]],
+		[[0, 1], [0, 1]],
+		[[0, 1], [1, 0]],
+		[[0, 1], [1, 1]],
+		[[1, 0], [0, 0]],
+		[[1, 0], [0, 1]],
+		[[1, 0], [1, 0]],
+		[[1, 0], [1, 1]],
+		[[1, 1], [0, 0]],
+		[[1, 1], [0, 1]],
+		[[1, 1], [1, 0]],
+		[[1, 1], [1, 1]],
 	]
 	
 	def play(self, game, bit1, bit2):
@@ -32,15 +32,15 @@ class mesh:
 		for i in range(16):
 			self.bit.append(random.randint(0, 1))
 			
-	def play(self):
+	def play(self, other):
 		game = self.bit[0] + (2 * self.bit[1]) + (4 * self.bit[2]) + (8 * self.bit[3])
-		inaddr1 = (self.bit[4] + (2 * self.bit[5]) + (4 * self.bit[6]) + (8 * self.bit[7]))
-		inaddr2 = (self.bit[8] + (2 * self.bit[9]) + (4 * self.bit[10]) + (8 * self.bit[11]))
-		outaddr = (self.bit[12] + (2 * self.bit[13]) + (4 * self.bit[14]) + (8 * self.bit[15]))
-		in1 = self.bit[inaddr1]
-		in2 = self.bit[inaddr2]
+		inaddr1 = self.bit[4] + (2 * self.bit[5]) + (4 * self.bit[6]) + (8 * self.bit[7])
+		inaddr2 = self.bit[8] + (2 * self.bit[9]) + (4 * self.bit[10]) + (8 * self.bit[11])
+		outaddr = self.bit[12] + (2 * self.bit[13]) + (4 * self.bit[14]) + (8 * self.bit[15])
+		in1 = other.bit[inaddr1]
+		in2 = other.bit[inaddr2]
 		out = game2.play(game, in1, in2)
-		self.bit[outaddr] = out
+		other.bit[outaddr] = out
 		
 	def print(self):
 		for i in range(16):
